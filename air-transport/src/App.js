aa283053da41b202e0f2e90580d9b0a39cbb64de
Sebastian Rychert <sebastian_rychert@wp.pl>
2022-05-21 14:58:32 +0200

    added color palette

--- air-transport/src/App.js
+++ air-transport/src/App.js
@@ -1,7 +1,34 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Container } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#e1ffb1',
+      main: '#aed581',
+      dark: '#7da453',
+      contrastText: '#000000',
+    },
+    secondary: {
+      light: '#e2f1f8',
+      main: '#b0bec5',
+      dark: '#808e95',
+      contrastText: '#f5f5f5',
+    },
+    background: {
+      default: "#e6e6e6",
+    },
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Container>
+        </Container>
+      </div>
+    </ThemeProvider>
   );
 }
 
