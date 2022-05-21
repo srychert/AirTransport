6dee2bb1f86faddcf89d86ac38569484e214c4f3
Sebastian Rychert <sebastian_rychert@wp.pl>
2022-05-21 20:02:40 +0200

    added InputProps and labelText

--- air-transport/src/FormComponents/MyInput.js
+++ air-transport/src/FormComponents/MyInput.js
@@ -1,11 +1,11 @@
 import { FormControl, TextField } from "@mui/material";
 import React from "react";
 
-function MyInput({ field, form, ...props }) {
+function MyInput({ field, form, InputProps, labelText, ...props }) {
   const fieldName = field.name;
-  const labelName = fieldName.replace("_", " ");
-  const withError = <TextField label={labelName} error helperText={form.errors[fieldName]} {...field} {...props} />;
-  const normal = <TextField label={labelName} {...field} {...props} />;
+  const labelName = labelText ? labelText : fieldName.replace("_", " ");
+  const withError = <TextField label={labelName} error helperText={form.errors[fieldName]} InputProps={InputProps} {...field} {...props} />;
+  const normal = <TextField label={labelName} InputProps={InputProps} {...field} {...props} />;
   return <FormControl fullWidth>{form.errors[fieldName] && form.touched[fieldName] ? withError : normal}</FormControl>;
 }
 
