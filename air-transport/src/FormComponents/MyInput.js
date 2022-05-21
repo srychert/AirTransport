0a4008f1a27a0b2e2a2e9e714727b09e8ca7207d
Sebastian Rychert <sebastian_rychert@wp.pl>
2022-05-21 21:16:29 +0200

    fixed label name

--- air-transport/src/FormComponents/MyInput.js
+++ air-transport/src/FormComponents/MyInput.js
@@ -2,8 +2,9 @@ import { FormControl, TextField } from "@mui/material";
 import React from "react";
 
 function MyInput({ field, form, InputProps, labelText, ...props }) {
-  const fieldName = field.name.charAt(0).toUpperCase() + field.name.slice(1);
-  const labelName = labelText ? labelText : fieldName.replace("_", " ")
+  const fieldName = field.name;
+  let labelName = labelText ? labelText : fieldName.replace("_", " ")
+  labelName = labelName.charAt(0).toUpperCase() + labelName.slice(1)
   const withError = <TextField label={labelName} error helperText={form.errors[fieldName]} InputProps={InputProps} {...field} {...props} />;
   const normal = <TextField label={labelName} InputProps={InputProps} {...field} {...props} />;
   return <FormControl fullWidth>{form.errors[fieldName] && (form.touched[fieldName] || form.touched.cargos) ? withError : normal}</FormControl>;
