b122f30e544177ba0e263c9e4f961fdab79d14bb
Sebastian Rychert <sebastian_rychert@wp.pl>
2022-05-22 14:25:46 +0200

    layout change to grid

--- air-transport/src/FormComponents/Cargo.js
+++ air-transport/src/FormComponents/Cargo.js
@@ -4,40 +4,48 @@ import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { Box, InputAdornment } from '@mui/material';
+import { Grid, InputAdornment } from '@mui/material';
 import MyInput from './MyInput';
 import MySelect from './MySelect';
 
-function Cargo({ array, cargo, typesOfCargo, cargoTemplate, index }) {
+function Cargo({ array, cargo, typesOfCargo, cargoTemplate, index, size }) {
   return (
-    <Box key={index} sx={{ display: "flex", flexDirection: "row" }}>
-      <Field name={`cargos.${index}.name`} component={MyInput} labelText="Name" />
-      <Field name={`cargos.${index}.weight`} type="number" component={MyInput} labelText="Weight"
-        InputProps={{
-          endAdornment: <InputAdornment position="end">kg</InputAdornment>,
-        }} />
-      <Field name={`cargos.${index}.type`} as="select" component={MySelect} items={typesOfCargo} labelText="Type" />
-      <Stack direction="row" spacing={0}>
-        <IconButton aria-label="add" color="success" onClick={() => array.insert(index, cargoTemplate())}
-          sx={{
-            "&:hover": {
-              color: "success.dark",
-            },
-          }}
-        >
-          <AddCircleIcon />
-        </IconButton>
-        <IconButton disabled={array.form.values.cargos.length == 1} aria-label="delete" color="error" onClick={() => array.remove(index)}
-          sx={{
-            "&:hover": {
-              color: "error.dark",
-            },
-          }}
-        >
-          <DeleteIcon />
-        </IconButton>
-      </Stack>
-    </Box>
+    <Grid container spacing={2}>
+      <Grid item xs={size.xs} sm={size.sm} md={size.md} lg={size.lg} xl={size.xl}>
+        <Field name={`cargos.${index}.name`} component={MyInput} labelText="Name" />
+      </Grid>
+      <Grid item xs={size.xs} sm={size.sm} md={size.md} lg={size.lg} xl={size.xl}>
+        <Field name={`cargos.${index}.weight`} type="number" component={MyInput} labelText="Weight"
+          InputProps={{
+            endAdornment: <InputAdornment position="end">kg</InputAdornment>,
+          }} />
+      </Grid>
+      <Grid item xs={size.xs} sm={size.sm} md={size.md} lg={3.85} xl={3.85}>
+        <Stack direction="row" spacing={0}>
+          <Field name={`cargos.${index}.type`} as="select" component={MySelect} items={typesOfCargo} labelText="Type" />
+          <IconButton aria-label="add" color="success" onClick={() => array.insert(index, cargoTemplate())}
+            sx={{
+              padding: 1,
+              "&:hover": {
+                color: "success.dark",
+              },
+            }}
+          >
+            <AddCircleIcon />
+          </IconButton>
+          <IconButton disabled={array.form.values.cargos.length === 1} aria-label="delete" color="error" onClick={() => array.remove(index)}
+            sx={{
+              padding: 1,
+              "&:hover": {
+                color: "error.dark",
+              },
+            }}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </Stack>
+      </Grid>
+    </Grid>
   )
 }
 
