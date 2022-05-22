f675b72e1b6868c40841b28a1df8dc55582ad61b
Sebastian Rychert <sebastian_rychert@wp.pl>
2022-05-22 22:54:50 +0200

    msg html and text body

--- form-api/email.js
+++ form-api/email.js
@@ -19,14 +19,46 @@ async function email(bodyObj, files) {
     "boeing": "boeing@lemonmind.com",
   }
 
-  let adressTo = body.to === "Airbus A380" ? addresses.airbus : addresses.boeing
+  let adressTo = body.airplane === "Airbus A380" ? addresses.airbus : addresses.boeing
+
+  const { documents, cargos, ...rest } = body
+  const cargoHtml = JSON.parse(cargos).map(c => {
+    return `<tr>
+            <td>name:${c.name}&nbsp;</td>
+            <td>weight:${c.weight}&nbsp;</td>
+            <td>type:${c.type}&nbsp;</td>
+            </tr>`
+  }).join('');
+
+  const cargoPlainText = JSON.parse(cargos).map(c => {
+    return `name:${c.name} weight:${c.weight} type:${c.type}\n`
+  }).join('');
+
+  const emailHTml = `
+    <p>from:${rest.from}</p>
+    <p>to:${rest.to}</p>
+    <p>airplane:${rest.airplane}</p>
+    <table>
+    <tbody>
+    ${cargoHtml}
+    </tbody>
+    </table>`
+
+  const emailPlainText = `
+  from:${rest.from}
+
+  to:${rest.to}
+
+  airplane:${rest.airplane}
+
+  ${cargoPlainText}`
 
   let info = await transporter.sendMail({
     from: '<api@example.com>',
     to: adressTo,
     subject: "Form",
-    text: JSON.stringify(body), // plain text body
-    html: JSON.stringify(body), // html body
+    text: emailPlainText,
+    html: emailHTml,
     attachments: files.map(file => {
       const fileAttachment = {
         path: file.path,
