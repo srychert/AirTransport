f3f06ee8505a71cb725dae81bebefe9b918abdd2
Sebastian Rychert <sebastian_rychert@wp.pl>
2022-05-22 19:43:19 +0200

    code clean-up

--- form-api/index.js
+++ form-api/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const bodyParser = require('body-parser');
 const multer = require('multer');
 const upload = multer({ dest: "uploads/" });
 const form = require('./routes/form');
@@ -10,17 +9,10 @@ const port = 5000
 
 app.use(cors())
 
-// for parsing application/json
-// app.use(express.json());
-
 // for parsing application/xwww-
-// app.use(bodyParser.urlencoded({ extended: true }));
-//form-urlencoded
-
-// for parsing multipart/form-data
 app.use(express.urlencoded({ extended: true }));
+// for parsing multipart/form-data
 app.use(upload.any("filesList"));
-// app.use(express.static('public'));
 
 app.use("/api/form", form);
 
